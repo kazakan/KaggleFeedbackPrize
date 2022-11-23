e1fb1ac810dbf3a34328588f568d991663ab1cde
Jh Do <dojh99@gmail.com>
2022-11-23 13:50:30 +0900

    FEAT: train with arg

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -1,27 +1,54 @@
+from os import PathLike
+from typing import Optional
 import torch
 import pytorch_lightning as pl
+from pathlib import Path
+from argparse import ArgumentParser
 
 from transformers import T5Tokenizer
 from ..data.dataset import FeedbackPrizeDataModule
 from ..models.EncT5 import EncT5MultiRegressModel
-from pathlib import Path
 
-
-if __name__ == "__main__":
-    
+def train(
+    train_path : PathLike,
+    max_epochs : int = 10,
+    batch_size : int = 32,
+    model_save_path : Optional[PathLike] = './model.pt',
+    tokenizer_save_path : Optional[PathLike] = './tokenizer.pt'
+):
     pl.seed_everything(42)
 
     model = EncT5MultiRegressModel()
     
     tokenizer = T5Tokenizer.from_pretrained("t5-base")
     datamodule = FeedbackPrizeDataModule(
-        Path('./data/train.csv'),Path('./data/test.csv'),
+        train_path=train_path,
         tokenizer=tokenizer,
-        batch_size=1
+        batch_size=batch_size
     )
-    trainer = pl.Trainer(precision=32,max_epochs=10)
+    trainer = pl.Trainer(precision=32,max_epochs=max_epochs)
 
     trainer.fit(model,datamodule)
 
-    torch.save(model,'./out/model.pt')
-    torch.save(tokenizer,'./out/tokenizer.pt')
+    torch.save(model,model_save_path)
+    torch.save(tokenizer,tokenizer_save_path)
+
+
+if __name__ == "__main__":
+    
+    parser = ArgumentParser()
+    parser.add_argument('train_path',type=Path)
+    parser.add_argument('--max_epochs',type=int,default=10)
+    parser.add_argument('--batch_size',type=int,default=32)
+    parser.add_argument('--model_save_path',type=Path,default='./model.pt')
+    parser.add_argument('--tokenizer_save_path', type=Path,default='./tokenizer.pt')
+
+    args = parser.parse_args()
+
+    train(
+        args.train_path,
+        args.max_epochs,
+        args.batch_size,
+        args.model_save_path,
+        args.tokenizer_save_path
+    )
